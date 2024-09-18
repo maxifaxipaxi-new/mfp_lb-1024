5afa593f171dcffbdb7827ba7baea9d3d943a5bf
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2024-09-18 10:34:12 +0200

    initial commit

---
+++ mfp_lb-1024/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.name = "1024" -- Name of the App
+Config.description = "Revived 1024x2!" -- disc in the appstore (edit for your local)
+Config.defaultApp = false -- OPTIONAL if set to true, app should be added without having to download it,
+Config.size = 4932 -- OPTIONAL in kb
+Config.price = 0 -- OPTIONAL, Make players pay with in-game money to download the app
+
+
+-- Support on discord.gg/mfp if needed. Check out issues on github. --
